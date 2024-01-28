e118d46906941b134ab759da951576f9ec7caf91
bri <bri@bri-macbook>
2024-01-28 16:33:50 -0500

    add minimal template from nix-darwin-kickstarter

---
+++ nix/minimal/flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "Nix for macOS configuration";
+
+  ##################################################################################################################
+  #
+  # Want to know Nix in details? Looking for a beginner-friendly tutorial?
+  # Check out https://github.com/ryan4yin/nixos-and-flakes-book !
+  #
+  ##################################################################################################################
+
+  # the nixConfig here only affects the flake itself, not the system configuration!
+  nixConfig = {
+    substituters = [
+      # Query the official cache first, and then the mirror of USTC.
+      "https://cache.nixos.org"
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+    ];
+  };
+
+  # This is the standard format for flake.nix. `inputs` are the dependencies of the flake,
+  # Each item in `inputs` will be passed as a parameter to the `outputs` function after being pulled and built.
+  inputs = {
+    # nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-23.11-darwin";
+    darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs-darwin";
+    };
+  };
+
+  # The `outputs` function will return all the build results of the flake.
+  # A flake can have many use cases and different types of outputs,
+  # parameters in `outputs` are defined in `inputs` and can be referenced by their names.
+  # However, `self` is an exception, this special parameter points to the `outputs` itself (self-reference)
+  # The `@` syntax here is used to alias the attribute set of the inputs's parameter, making it convenient to use inside the function.
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    darwin,
+    ...
+  }: let
+    username = "bri";
+    system = "x86_64-darwin"; # aarch64-darwin or x86_64-darwin
+
+    hostname = "${username}-macbook";
+    specialArgs =
+      inputs
+      // {
+        inherit username hostname;
+      };
+  in {
+    darwinConfigurations."${hostname}" = darwin.lib.darwinSystem {
+      inherit system specialArgs;
+      modules = [
+        ./modules/nix-core.nix
+        ./modules/system.nix
+        ./modules/apps.nix
+
+        ./modules/host-users.nix
+      ];
+    };
+    # nix code formatter
+    formatter.${system} = nixpkgs.legacyPackages.${system}.alejandra;
+  };
+}
---
+++ nix/minimal/modules/apps.nix
@@ -0,0 +1,86 @@
+{ pkgs, ...}: {
+
+  ##########################################################################
+  #
+  #  Install all apps and packages here.
+  #
+  #  NOTE: Your can find all available options in:
+  #    https://daiderd.com/nix-darwin/manual/index.html
+  #
+  # TODO Fell free to modify this file to fit your needs.
+  #
+  ##########################################################################
+
+  # Install packages from nix's official package repository.
+  #
+  # The packages installed here are available to all users, and are reproducible across machines, and are rollbackable.
+  # But on macOS, it's less stable than homebrew.
+  #
+  # Related Discussion: https://discourse.nixos.org/t/darwin-again/29331
+  environment.systemPackages = with pkgs; [
+    git
+    rnix-lsp
+  ];
+
+  # The apps installed by homebrew are not managed by nix, and not reproducible!
+  # But on macOS, homebrew has a much larger selection of apps than nixpkgs, especially for GUI apps!
+  homebrew = {
+    enable = true;
+
+    onActivation = {
+      autoUpdate = true;
+      # 'zap': uninstalls all formulae(and related files) not listed here.
+      # cleanup = "zap";
+    };
+
+    taps = [
+      "homebrew/cask-fonts"
+      "homebrew/services"
+      "homebrew/cask-versions"
+    ];
+
+    # `brew install`
+    # TODO Feel free to add your favorite apps here.
+    brews = [
+      # "aria2"  # download tool
+      # "atuin"
+      # "bash"
+      # "bat"
+      # "ca-certificates"
+      # "direnv"
+      # "gettext"
+      # "kubernetes-cli"
+      # "libgit2"
+      # "libssh2"
+      # "libtermkey"
+      # "libuv"
+      # "libvterm"
+      # "luajit"
+      # "luv"
+      # "mpdecimal"
+      # "msgpack"
+      # "neovim"
+      # "nixpacks"
+      # "oniguruma"
+      # "openssl@3"
+      # "python@3.12"
+      # "readline"
+      # "six"
+      # "sqlite"
+      # "starship"
+      # "stow"
+      # "thefuck"
+      # "tree-sitter"
+      # "unibilium"
+      # "xz"
+    ];
+
+    # `brew install --cask`
+    # TODO Feel free to add your favorite apps here.
+    casks = [
+      # "google-chrome"
+      "1password-cli"
+      "iterm2"
+    ];
+  };
+}
